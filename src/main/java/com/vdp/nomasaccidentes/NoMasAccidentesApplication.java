470c0710f2e0c7c6d1f34b78cdbc78282984b73a
Sebastian Lara <pelukdev@gmail.com>
2022-01-10 21:06:54 -0300

    Start project

---
+++ src/main/java/com/vdp/nomasaccidentes/NoMasAccidentesApplication.java
@@ -0,0 +1,13 @@
+package com.vdp.nomasaccidentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoMasAccidentesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NoMasAccidentesApplication.class, args);
+  }
+
+}
